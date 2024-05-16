f4bafe4b1ab2dc9a62bb8794548a35fba6745c9a
hayashi8131 <164457933+hayashi8131@users.noreply.github.com>
2024-05-16 12:57:51 +0800

    Update url.js

--- url.js
+++ url.js
@@ -1,16 +1,4 @@
-/*
- * 將要隨機派發的網址放入下方（不需排序）
- *
- * 並請注意：
- * 1. 除最後一項外，其餘項目用逗號分隔
- * 2. 網址請用引號包起來
- */
-
-
 const urls = [
-	
-	'https://forms.gle/xWaNQkfV2TRQ73Vw9',
-	'https://forms.gle/1ygFxK8SLUUkubit9',
-	'https://forms.gle/w2y2faY5WVsBsAZR6',
-	'https://forms.gle/zAYSsUuPEViZEvZG6'
+    'https://forms.gle/1ygFxK8SLUUkubit9',
+    'https://forms.gle/w2y2faY5WVsBsAZR6'
 ];
